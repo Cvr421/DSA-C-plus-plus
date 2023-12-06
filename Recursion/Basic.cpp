66062d13ebe1f8f502ae20812ad3b5ad5df413eb
chandravijay <chandravijayk42187@gmail.com>
2023-12-06 14:27:00 +0530

    Basic recursion

---
+++ Recursion/Basic.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int recusion(int n){
+    if( n>0){
+       
+        return n-1 ;
+         cout<<"hello world"<<endl;
+    }
+}
+int main(){
+   recusion(5);
+    return 0;   
+}
